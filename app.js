9120ed61d149b998a730e4539c84abae6901ed35
Bethany White <bethanypatriciaw@gmail.com>
2025-04-28 13:58:28 +0100

    completed task 3

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const express = require("express")
 const app = express()
 const { getApi } = require("./app/controllers/api.controller")
 const { getTopics } = require("./app/controllers/topics.controller")
-const { internalServerError } = require("./app/controllers/error.controller")
+const { customErrorHandler, internalServerError, sqlErrorHandler } = require("./app/controllers/error.controller")
+const { getArticleById } = require("./app/controllers/articles.controller")
 
 
 app.get("/api", getApi)
 
 app.get("/api/topics", getTopics)
 
+app.get("/api/articles/:article_id", getArticleById)
 
+app.use(sqlErrorHandler)
+app.use(customErrorHandler)
 
 app.use(internalServerError)
 
