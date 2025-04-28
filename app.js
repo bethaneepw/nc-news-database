44ec1285ceeae084744d58b4d7854271c454a2d0
Bethany White <bethanypatriciaw@gmail.com>
2025-04-28 15:41:35 +0100

    completed kata 4

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const app = express()
 const { getApi } = require("./app/controllers/api.controller")
 const { getTopics } = require("./app/controllers/topics.controller")
 const { notFoundHandler, customErrorHandler, internalServerError, sqlErrorHandler } = require("./app/controllers/error.controller")
-const { getArticleById } = require("./app/controllers/articles.controller")
+const { getArticleById, getArticles } = require("./app/controllers/articles.controller")
 
 
 app.get("/api", getApi)
 
 app.get("/api/topics", getTopics)
 
+app.get("/api/articles", getArticles)
+
 app.get("/api/articles/:article_id", getArticleById)
-
-
 
 app.use(sqlErrorHandler)
 app.use(customErrorHandler)
