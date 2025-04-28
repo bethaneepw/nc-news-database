05d06c009e3abcd85cae85b9ac7eaf54099d9ce9
Bethany White <bethanypatriciaw@gmail.com>
2025-04-28 10:42:27 +0100

    completed task 1

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+const { getApi } = require("./app/controllers/api.controller")
+
+app.get("/api", getApi)
+
+
+
+
+
+
+
+module.exports = app;
