1f105a0b10f20fb47fdeec5f4b7f47ab7e6a290d
Bethany White <bethanypatriciaw@gmail.com>
2025-04-30 09:33:43 +0100

    completed kata 9

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -385,6 +385,24 @@ describe("DELETE: /api/comments/:comment_id", () => {
   })
 })
 
+describe("GET: /api/users", () => {
+  test("200: Responds with an array of user objects", () => {
+    return request(app)
+    .get("/api/users")
+    .expect(200)
+    .then(({body: { users }}) => {
+      expect(users).toHaveLength(4)
+      users.forEach((user) => {
+        expect(user).toMatchObject({
+          username: expect.any(String),
+          name: expect.any(String),
+          avatar_url: expect.any(String)
+      });
+    })
+  })
+})
+})
+
 describe("ERROR invalid endpoint", () => {
   test("404: Responds with error if user inputs an invalid endpoint", () => {
     return request(app)
